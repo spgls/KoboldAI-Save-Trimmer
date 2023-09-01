88ac54b3781a3b4a48d96ef7f9021550e4e5de47
spgls <spgls123@gmail.com>
2023-09-01 19:04:09 +0300

    Update app.py
    The program now self-cleans its directories of junk files

--- flask-web/app.py
+++ flask-web/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, redirect, send_file, send_from_directory
-import json, os
+import json, os, time, threading
 
 app = Flask(__name__)
+
+def delete_json_files():
+    upload_dir = './uploads'
+    for filename in os.listdir(upload_dir):
+        if filename.endswith('.json'):
+            os.remove(os.path.join(upload_dir, filename))
+
+    current_dir = './'
+    for filename in os.listdir(current_dir):
+        if filename.endswith('.json') and "trimmed_" in filename:
+            os.remove(os.path.join(current_dir, filename))
 
 def trim_text(data, amount=10000):
     data['actions_metadata'] = {}
@@ -20,6 +31,10 @@ def trim_text(data, amount=10000):
 
     return data
 
+def delete_file_with_delay(filename):
+    time.sleep(60)
+    os.remove(filename)
+
 @app.route('/', methods=['GET', 'POST'])
 def trim_web():
     if request.method == 'POST':
@@ -32,6 +47,10 @@ def trim_web():
             trimmed_filename = "trimmed_" + file.filename
             with open(trimmed_filename, 'w', encoding='utf-8') as trimmed_file:
                 json.dump(trimmed_data, trimmed_file, ensure_ascii=False, indent=4)
+
+            delete_thread = threading.Thread(target=delete_file_with_delay, args=(trimmed_filename,))
+            delete_thread.start()
+
             return send_file(trimmed_filename, as_attachment=True)
     
     return render_template('index.html')
@@ -53,4 +72,5 @@ def icon(icon_filename):
     
 
 if __name__ == '__main__':
+    delete_json_files()
     app.run(host='0.0.0.0', port=5002, debug=False)
